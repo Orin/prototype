a69f01806373dbde42bb3100c63a074f9b8d00fb
Michael Shannon <michaelrshannon@gmail.com>
2011-03-23 18:28:52 +0000

    Added force HTTPS to both front end and staffmanager.

--- front_end/index.php
+++ front_end/index.php
@@ -1,3 +1,9 @@
+<?php
+//Switch to https and reset to home if not on https
+if (!$_SERVER['HTTPS']) { 
+	header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
--- front_end/staffmanager/index.php
+++ front_end/staffmanager/index.php
@@ -1,4 +1,8 @@
 <?php
+//Force HTTPS
+if (!$_SERVER['HTTPS']) { 
+	header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+}
 //Include all functions
 include '../config/definitions.inc.php';
 include '../config/functions.inc.php';
