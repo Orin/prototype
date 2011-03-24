7880ac6e393c29b0155446c4cd70ae7efe0cb2f6
Mathieu Saillet <ms498@linux12.(none)>
2011-03-24 16:04:45 +0000

    test file

--- front_end/test.php
+++ front_end/test.php
@@ -1,4 +1,20 @@
-
+<script type="text/javascript" >
+	function verifMail(formulaire){
+	email=formulaire.email.value;
+	var place=email.indexOf(@,1);
+	var dot=email.indexOf(".",place+1);
+	if ((dot > -1)&&(email.length >2)&&(dot > 1))
+		{
+		formulaire.submit();
+		return(true);
+		}
+	else
+		{
+		alert('Give a valid email!');
+		return(false);
+		}
+	}
+</script>
  <form action="" name="vaild" method="post" onSubmit="return(VerifForm(this))">
 		<table id="details">
           
