11bde1efdf8b0bb40b30ede7194db9ed38ae3967
Mathieu Saillet <ms498@linux12.(none)>
2011-03-24 15:46:21 +0000

    validation for company website

---
+++ company/assets/js/validation.js
@@ -0,0 +1,16 @@
+//function which check the email is correct or not. If not it gives an alert.
+function verifMail(formulaire){
+	email=formulaire.email.value;
+	var place=email.indexOf(@,1);
+	var dot=email.indexOf(".",place+1);
+	if ((dot > -1)&&(email.length >2)&&(dot > 1))
+		{
+		formulaire.submit();
+		return(true);
+		}
+	else
+		{
+		alert('Give a valid email!');
+		return(false);
+		}
+	}
