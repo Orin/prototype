d6a19bbe4846e14cfed10954846acafa4e53e55f
Mathieu Saillet <ms498@linux12.(none)>
2011-03-24 16:20:57 +0000

    test file

--- front_end/test.php
+++ front_end/test.php
@@ -1,24 +1,20 @@
-<script type="text/javascript" >
-	function verifMail(formulaire){
-	adresse=formulaire.email.value;
-	var place=adresse.indexOf(@,1);
-	var dot=adresse.indexOf(".",place+1);
-	if ((dot > -1)&&(adresse.length >2)&&(dot > 1))
-		{
-		formulaire.submit();
-		return(true);
-		}
-	else
-		{
-		alert('Give a valid email!');
-		return(false);
-		}
+<script type="text/javascript">
+	function validate(form_id,email) {
+		   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		   var address = document.forms[form_id].elements[email].value;
+		   if(reg.test(address) == false) {
+			  alert('Invalid Email Address');
+			  return false;
+		   }
 	}
+	
 </script>
- <form action="http://www2.macs.hw.ac.uk/~ms498/prototype/front_end/test.php" name="vaild" method="post" onSubmit="return(VerifForm(this))">
+
+ <form id="test" method="post" onSubmit="javascript:return validate('form_id','email');">
 		<table id="details">
           
    <tr class="email"><td>Email Address</td><td><input type="text" name="email" />
         </table>
     <input type="submit" name="submit" value="submit" />
        </form>
+
