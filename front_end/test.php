705f243f8c68ff7066977ebd0afa172613c7ccc6
Mathieu Saillet <ms498@linux12.(none)>
2011-03-24 17:12:22 +0000

    test

--- front_end/test.php
+++ front_end/test.php
@@ -1,5 +1,5 @@
-<script type="text/javascript">	function validate(form_id,email) {
-		   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+<script type="text/javascript">	function validate(form_id,ref) {
+		   var reg = /^([I-IO-O0-0_\-\.])$/;
 		   var address = document.forms[form_id].elements[email].value;
 		   if(reg.test(address) == false) {
 			  alert('Invalid Email Address');
@@ -10,7 +10,7 @@
  <form id="test" method="post" onSubmit="javascript:return validate('test','email');">
 		<table id="details">
           
-   <tr class="email"><td>Email Address</td><td><input type="text" name="email" />
+   <tr class="email"><td>Email Address</td><td><input type="text" name="ref" />
         </table>
     <input type="submit" name="submit" value="submit" onBlur="javascript:return validate('test','email');"/>
        </form>
