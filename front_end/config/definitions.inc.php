a0734755bdff948b68fc875a87297617dd34d91e
Michael Shannon <michaelrshannon@gmail.com>
2011-03-24 00:30:44 +0000

    Added OVERBOOK_MOD definition to front_end/config/definitions.inc.php to define the overbooking modifier (currently set to 1.1)

--- front_end/config/definitions.inc.php
+++ front_end/config/definitions.inc.php
@@ -1,5 +1,6 @@
 <?php
 define ("SITE_NAME", "Thistle Airways");
+define ("OVERBOOK_MOD", 1.1);
 
 $destinations = array('', 'Edinburgh', 'Glasgow', 'Aberdeen', 'Inverness');
 $classes = array('Economy', 'Business');
