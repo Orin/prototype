4f1e11535b8cad3426b53ecae02a8d212bb4a1cf
Mathieu Saillet <ms498@linux38.(none)>
2011-03-28 13:09:29 +0100

    Work on test

--- front_end/test.php
+++ front_end/test.php
@@ -1,20 +1,27 @@
-<script type="text/javascript">	function validate(form_id,ref){
+<script type="text/javascript">	function validate(form_id,ref)
+{
 		   var reg1 = /^.{6}$/;
 		   var reg = /[2-9]{3,3}$/;
 		   var reg2=/^[A-HJ-NP-Z]{3,3}/;
 		   var address = document.forms[form_id].elements[ref].value;
-		   if(reg1.test(address)==true){
-			   if(reg.test(address)==true) {
-				 if(reg2.test(address)==false){
-					 alert('pas bon ');
-					 return false;
-				}else 
-					alert('jojufwi');
+		   if(reg1.test(address)==true)
+				{
+				if(reg.test(address)==true) 
+					{
+					if(reg2.test(address)==false)
+						{
+						alert('pas bon ');
+						return false;
+						}
+					else 
+						alert('bonne lettre');
 				else
-				alert('ok');
-		   }else
-				alert('Encule');
-	}</script>
+					alert('pas chiffre');
+				}
+			else
+				alert('pas 6');
+}
+</script>
 
  <form id="test" method="post" onSubmit="javascript:return validate('test','ref');">
 		<table id="details">
